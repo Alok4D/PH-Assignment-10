9c1d61a2cb87d6ef4d53a5e7cba5269e50c99fb8
Alok4D <alokroy602701@gmail.com>
2024-04-29 23:59:26 +0600

    add dark and light mode

--- src/Components/AllArtAndCraftItem/AllCard.jsx
+++ src/Components/AllArtAndCraftItem/AllCard.jsx
@@ -9,10 +9,12 @@ const AllCard = () => {
     return (
 
         <div>
-            <div>{allCard.length}</div>
-          {
+            <div className="flex justify-center text-red-600">{allCard.length}</div>
+        <div className="grid sm:grid-cols-1 lg:grid-cols-3 gap-5 lg:w-[80%] mx-auto" data-aos="zoom-in">
+            {
             allCard.map(allCard => <ShowCard allCard={allCard} key={allCard._id}></ShowCard>)
-          }
+            }
+        </div>
         </div>
     );
 };
